edab675fd98a073634d40cc37ffa1bd8c192f0da
Viktor Kukurba <viktor.kukurba@gmail.com>
2019-08-05 17:13:19 +0300

    v0.1.17

---
+++ WRC-chrome/extension/background.js
@@ -0,0 +1,7 @@
+(function() {
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function(tab) {
+      console.log('tab opened', tab);
+    });
+  });
+})()
