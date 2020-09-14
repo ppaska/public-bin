2f4fafbd7eec4b470ad19de3e7ecd1b82a06132a
Pavlo Paska <ppaska@gmail.com>
2020-09-14 22:08:27 +0100

    local

--- WRC-chrome/xml.js
+++ WRC-chrome/xml.js
@@ -1,1 +1,1 @@
-ace.define("ace/snippets/xml",["require","exports","module"],(function(e,o,t){"use strict";o.snippetText=void 0,o.scope="xml"})),ace.require(["ace/snippets/xml"],(function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)}));
+ace.require(["ace/snippets/xml"],(function(e){"object"==typeof module&&"object"==typeof exports&&module&&(module.exports=e)}));
